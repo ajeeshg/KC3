8d34c589a33fc33e1cfb9b819578a0e49ad382e1
Ajeesh <naira@campusad.msu.edu>
2012-07-17 09:50:10 -0400

    Initial KC New Version

---
+++ KC3/src/com/test/KcMain.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class KcMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//This is a test class
+	}
+
+}
